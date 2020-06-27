bee8381185d539db48ab2c0af734c0e14269e1f3
GDPlumb <gdplumb@andrew.cmu.edu>
2020-06-27 09:59:10 -0400

    Added a transfer learned model for COCO

--- Environment/environment.sh
+++ Environment/environment.sh
@@ -1,4 +1,6 @@
 
 conda env remove --name countervision
 conda env create -f environment.yml
+source activate countervision
+conda install -c conda-forge pycocotools=2.0.*
 conda list --name countervision
