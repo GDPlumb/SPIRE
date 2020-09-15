4de1d5a73389e68d602fd44bb337360517f872ee
GDPlumb <gdplumb@andrew.cmu.edu>
2020-09-15 08:46:17 -0400

    Expanded the analysis to include all of the strong spurious correlations involving 'person'

--- COCO/Plot.sh
+++ COCO/Plot.sh
@@ -7,9 +7,11 @@ source /home/gregory/anaconda3/etc/profile.d/conda.sh
 
 conda activate countervision
 
-for main_class in 'skis' 'frisbee' 'tennis-racket' 'bicycle'
+for main_class in 'snowboard' 'couch' 'tie' 'handbag' 'skis' 'remote' 'toothbrush' 'sports-ball' 'knife' 'cell-phone' 'fork' 'wine-glass' 'skateboard' 'spoon' 'backpack' 'bench' 'frisbee'
 do
+    echo $main_class
     python Plot.py $root $year $main_class $spurious_class
+    echo ''
 done
 
 conda deactivate
