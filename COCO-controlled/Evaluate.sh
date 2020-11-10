82e2e501ffb49d479c56380465dc88e2c6493683
GDPlumb <gplumb@wisc.edu>
2020-11-10 13:37:01 -0500

    Changed number of trials to run while testing

--- COCO-controlled/Evaluate.sh
+++ COCO-controlled/Evaluate.sh
@@ -4,8 +4,8 @@ source /home/gregory/anaconda3/etc/profile.d/conda.sh
 
 conda activate countervision
 
-CUDA_VISIBLE_DEVICES=0 python Evaluate.py $1 $2 $3 $4 0,1,2 &
-CUDA_VISIBLE_DEVICES=1 python Evaluate.py $1 $2 $3 $4 3,4,5 &
+CUDA_VISIBLE_DEVICES=0 python Evaluate.py $1 $2 $3 $4 0,2 &
+CUDA_VISIBLE_DEVICES=1 python Evaluate.py $1 $2 $3 $4 1,3 &
 
 wait
 
