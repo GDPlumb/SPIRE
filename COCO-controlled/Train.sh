00b2503fe9643b8bf1b5d158af0d70ac71c7ab7b
GDPlumb <gplumb@wisc.edu>
2020-11-18 08:54:36 -0500

    Updated GPU workers

--- COCO-controlled/Train.sh
+++ COCO-controlled/Train.sh
@@ -4,8 +4,10 @@ source /home/gregory/anaconda3/etc/profile.d/conda.sh
 
 conda activate countervision
 
-CUDA_VISIBLE_DEVICES=0 python Train.py $1 $2 $3 $4 0,2 &
-CUDA_VISIBLE_DEVICES=1 python Train.py $1 $2 $3 $4 1,3 &
+CUDA_VISIBLE_DEVICES=0 python Train.py $1 $2 $3 $4 0 &
+CUDA_VISIBLE_DEVICES=1 python Train.py $1 $2 $3 $4 1 &
+CUDA_VISIBLE_DEVICES=2 python Train.py $1 $2 $3 $4 2 &
+CUDA_VISIBLE_DEVICES=3 python Train.py $1 $2 $3 $4 3 &
 
 wait
 
