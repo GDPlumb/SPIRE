14dbd7dd946e8655a37daaaa709dcd7a42e70d28
GDPlumb <gplumb@wisc.edu>
2020-11-22 08:07:14 -0500

    Reran the updated Search.  Note:  some of the changes to SetupPair are untested and so the entire pipeline should be reran to ensure consistency.

--- COCO-controlled/Run.sh
+++ COCO-controlled/Run.sh
@@ -1,12 +1,12 @@
 
-SETUP=false
-TRAIN=true
-EVAL=true
+SETUP=true
+TRAIN=false
+EVAL=false
 SEARCH=true
 PLOT=true
 
 # 'bottle person' 'bowl person' 'car person' 'chair person' 'cup person' 'dining+table person' 'bottle cup' 'bowl cup' 'chair cup' 'bottle dining+table' 'bowl dining+table' 'chair dining+table' 'cup dining+table'
-for i in 'bottle person' 'bowl person' 'car person' 'chair person' 'cup person' 'dining+table person'
+for i in 'bowl person' 'car person' 'chair person' 'cup person' 'dining+table person' 
 do
     set -- $i
     main=$1
@@ -22,12 +22,12 @@ do
     ./SetupPair.sh $main $spurious
     fi
         
-    for mode in 'careful-tune'
+    for mode in 'initial-transfer' 'initial-tune' 'careful-tune' 'careful-paint-tune'
     do
         echo ''
         echo $mode
             
-        for p in 0.025 0.05 0.1 0.2 0.4
+        for p in 0.025 0.05 0.1 0.2 0.4 0.6 0.8 0.9 0.95 0.975
         do
             echo $p
             if $TRAIN ; then
